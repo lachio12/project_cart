a196826a974eabf0b3d95f0c4a740208754168e9
lachio12 <truongvanthong94@gmail.com>
2019-09-19 15:16:14 +0700

    Updates

---
+++ precache-manifest.0ff2d93a0a71d5788db09474b9497b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a80e5883b8bbb87758bd149fe071ad",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "312e2a548e512f8106d7",
+    "url": "/shopping_cart/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1c50f8482549c278f751",
+    "url": "/shopping_cart/static/js/2.bcbf78f6.chunk.js"
+  },
+  {
+    "revision": "312e2a548e512f8106d7",
+    "url": "/shopping_cart/static/js/main.ed9a17bb.chunk.js"
+  },
+  {
+    "revision": "09e6ee112c9a1cd47820",
+    "url": "/shopping_cart/static/js/runtime~main.d873d046.js"
+  }
+]);
